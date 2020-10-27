df530fe1a02a99fdea1592158ceb7c0daf238f53
David <sanderdj90@gmail.com>
2020-10-26 21:19:49 -0600

    feat: addPost mutation

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { graphql, Link } from "gatsby"
 import gql from "graphql-tag"
-import { useSubscription } from "react-apollo-hooks"
+import { useSubscription, useMutation } from "react-apollo-hooks"
 export const query = graphql`
   {
     blog {
@@ -29,14 +29,29 @@ const GET_POSTS = gql`
     }
   }
 `
+
+const ADD_POST = gql(`
+mutation addPost($content: String!, $title: String!) {
+  insert_posts(objects: {author_id: "135c3857-341f-4446-89f5-5ad30a24ed37", content: $content, title: $title}) {
+  returning {
+    title
+  }
+  }
+}
+
+`)
 export default function Home() {
   const { data, loading, error } = useSubscription(GET_POSTS, {
     suspend: false,
   })
+  let input
+  const [addPost, { data2 }] = useMutation(ADD_POST)
   if (loading === true) {
     return <div>loading</div>
   }
-
+  if (error) {
+    return <div>{JSON.stringify(error, null, 2)}</div>
+  }
   if (loading === false && !error) {
     let posts = data.posts.map(post => {
       return (
@@ -50,11 +65,25 @@ export default function Home() {
         </article>
       )
     })
-    return <div>{posts}</div>
-
-    if(error) {
-      return <div>{JSON.stringify(error, null,2)}</div>
-    }
+    return (
+      <>
+        <div>{posts}</div>
+        <form
+          onSubmit={e => {
+            e.preventDefault()
+            addPost({ variables: { title: input.value, content: "derp" } })
+            input.value = ""
+          }}
+        >
+          <input
+            ref={node => {
+              input = node
+            }}
+          />
+          <button type="submit">Add Post</button>
+        </form>
+      </>
+    )
   }
   console.log(data, loading, error)
 }
